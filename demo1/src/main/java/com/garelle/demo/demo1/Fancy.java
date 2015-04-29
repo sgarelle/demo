ab8181e1ab204adc8a26e93d3f727a70b5d50f8d
Sebastien Garelle <sebastien.garelle@ge.com>
2015-04-29 18:37:28 +0200

    A more test, for my demo.

---
+++ demo1/src/main/java/com/garelle/demo/demo1/Fancy.java
@@ -0,0 +1,8 @@
+package com.garelle.demo.demo1;
+
+public class Fancy {
+
+    public static void main(String[] args) {
+        System.out.println("Fancy.main");
+    }
+}
---
+++ demo1/src/test/java/com/garelle/demo/demo1/FancyTest.java
@@ -0,0 +1,22 @@
+package com.garelle.demo.demo1;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FancyTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testName() throws Exception {
+    }
+}
