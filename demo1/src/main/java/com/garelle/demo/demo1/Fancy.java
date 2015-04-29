d7ec7775005d116096f0fb34769e8db349a4062a
sgarelle <seb@localhost>
2015-04-29 10:13:38 -0700

    Change log

--- demo1/src/main/java/com/garelle/demo/demo1/Fancy.java
+++ demo1/src/main/java/com/garelle/demo/demo1/Fancy.java
@@ -3,6 +3,6 @@ package com.garelle.demo.demo1;
 public class Fancy {
 
     public static void main(String[] args) {
-        System.out.println("Fancy.main - conflict 2");
+        System.out.println("Fancy.main");
     }
 }
